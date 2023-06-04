87cefe684c1a692ff73478f314b7fbbbd11f9369
Antonio <antonio.tabares@outlook.es>
2023-06-04 16:32:41 -0500

    Functions and data types exercices solved

---
+++ main.py
@@ -0,0 +1,8 @@
+from Topics import functions_n_types
+
+if __name__ == "__main__":
+  # e = functions_n_types.find_maximum([1, 4, 7, 5, 34, 723, 51])
+  # print(e)
+
+  f = functions_n_types.reverse_string('tangamandapio')
+  print(f)
